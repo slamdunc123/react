1ce000b531f6f93a9371a7945bba2185811311fc
slamdunc123 <slamdunc123@gmail.com>
2018-11-23 14:57:14 +0000

    Updates

---
+++ precache-manifest.63830653b4f5c89c1989258c67036c24.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d6d0aa42199878ab9859",
+    "url": "/react-tutorial/static/js/runtime~main.d6d0aa42.js"
+  },
+  {
+    "revision": "8fc507cdc21649d80df0",
+    "url": "/react-tutorial/static/js/main.8fc507cd.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/react-tutorial/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "8fc507cdc21649d80df0",
+    "url": "/react-tutorial/static/css/main.bfbaffd8.chunk.css"
+  },
+  {
+    "revision": "ec38a990092eba4e90a666577ec8db3d",
+    "url": "/react-tutorial/index.html"
+  }
+];
