a5d217cc90701cb279866ab1cf81ec693e189e38
slamdunc123 <slamdunc123@gmail.com>
2018-11-23 14:59:05 +0000

    Updates

---
+++ precache-manifest.f334162e32af04d0f0374ec3a4d7f72c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "132b963a467195a4a4b5",
+    "url": "/react-tutorial/test/static/js/runtime~main.132b963a.js"
+  },
+  {
+    "revision": "8fc507cdc21649d80df0",
+    "url": "/react-tutorial/test/static/js/main.8fc507cd.chunk.js"
+  },
+  {
+    "revision": "f68dcc1a0c0c6e129be0",
+    "url": "/react-tutorial/test/static/js/1.f68dcc1a.chunk.js"
+  },
+  {
+    "revision": "8fc507cdc21649d80df0",
+    "url": "/react-tutorial/test/static/css/main.bfbaffd8.chunk.css"
+  },
+  {
+    "revision": "8e897ed77147eea8cd1411ecc2fd3419",
+    "url": "/react-tutorial/test/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/react-tutorial/precache-manifest.63830653b4f5c89c1989258c67036c24.js"
+  "/react-tutorial/test/precache-manifest.f334162e32af04d0f0374ec3a4d7f72c.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/react-tutorial/index.html", {
+workbox.routing.registerNavigationRoute("/react-tutorial/test/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
